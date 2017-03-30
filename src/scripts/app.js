e6f9984e5f92b7f1c31adfe50e0e675f5b93f4ff
cr0wbait <etcothran@gmail.com>
2017-03-30 17:54:10 -0400

    what even

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery'
 
 console.log ("Windmill, windmill, through the sky!")
 
@@ -34,8 +35,12 @@ const BigDealContainer = React.createClass({
 	_makeABigDeal: function(everythings, index){
 		let component = this
 		let allThings = everythings.map(function(list, index){
+		let tellThings = document.querySelector('.tellThings')
 			return(
-				<item removeItem={component._oldThing} list={list} key={index}/>
+				<div className="item">
+					<p>what am i</p>
+					<button><i className="fa fa-asterisk"></i></button>
+				</div>
 			)
 		})
 		return allThings
@@ -46,10 +51,9 @@ const BigDealContainer = React.createClass({
 			<div className="bigdeal">
 				<h1>Do Me.</h1>
 					<InputThing refreshList={this._newThing}/>
-					<hr/>
 					<div className="items">
 						{this._makeABigDeal(this.state.doStuff)}
-						</div>
+					</div>
 					</div>
 		)
 	}
@@ -66,7 +70,7 @@ const InputThing = React.createClass({
 
 	render: function(){
 		return (
-			<div className="listies">
+			<div className="control">
 				<input ref="tellMe" type="text" className="tellThings" placeholder="Tell me what you want."></input>
 				<button className="punch_me" onClick={this._putNewThing}><i className="fa fa-crosshairs" aria-hidden="true"></i></button>
 			</div>
@@ -74,7 +78,7 @@ const InputThing = React.createClass({
 	}
 })
 
-const ThingToDo = React.createClass({
+const UnThing = React.createClass({
 	_throwStuffAway: function(){
 		this.props.removeItem(this.props.everythings.item)
 	},
